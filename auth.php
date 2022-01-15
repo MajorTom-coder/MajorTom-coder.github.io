bdb05d72929ad66a8a263bf0d96b0dd34b2b9efe
Nesmelov Pavel <82256905+MajorTom-coder@users.noreply.github.com>
2022-01-15 14:55:05 +0500

    BookShelter v1.3 ()
    A personal account page has been added and a script for logging in and out to the user's personal account is being developed.
    In the future, it will be necessary to display the login on all pages of the site, instead of "personal account".

--- auth.php
+++ auth.php
@@ -1,13 +1,24 @@
 <?php
     $user_name = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
-    $inst_name = filter_var(trim($_POST['inst_name']), FILTER_SANITIZE_STRING);
     $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
     $password = md5($password."i2fyoul5o5oki2nto15");
 
 
     $mysql =  new mysqli('localhost', 'root', '', 'register-bd'); # создание бд
-    $mysql->query("INSERT INTO `users` (`login`, `inst`, `password`) VALUES('$user_name','$inst_name','$password')"); #отправка данных в бд
+    $result = $mysql->query("SELECT * FROM 'users' WHERE 'login' = '$user_name' AND 'password' = '$password' ")
+
+    $user = $result->fetch_assoc();
+    if(count($user_name) == 0) {
+        echo "User is not defind";
+        exit();
+    }
+
+    
+    setcookie('user', $user_name['login'], time() + 3600);
+        
+        
+        
     $mysql->close(); #закрытие бд
 
     header('Location: /'); #переадресация
